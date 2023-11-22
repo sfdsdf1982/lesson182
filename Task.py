07b3717b65b51e71345a6b2ff6bedc12344a0d64
sfdsdf1982 <134068597+sfdsdf1982@users.noreply.github.com>
2023-11-22 13:20:50 +0300

    Update Task.py

--- Task.py
+++ Task.py
@@ -4,4 +4,4 @@
 def get_count(lst):
    return len([lst[i] for i in range(1,len(lst) - 1) if lst[i - 1] % 2 != 0 and lst[i + 1] % 2 != 0])
 
-print(get_count([11,2,3,4,5,7,3,1,9]))
+print(get_count([11,2,3,4,5,7,3,1,91]))
